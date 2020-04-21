64deb656bb093413e42cff06e765a80d43c39e68
rvilela7 <ruivil@gmail.com>
2020-04-21 23:32:25 +0100

    wip

---
+++ SportShop/SportShop.API/Models/ShopContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SportShop.API.Models
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products {get;set;}
+        public DbSet<Category> Categories {get;set;}
+        public DbSet<Order> Orders{get;set;}
+        public DbSet<User> Users{get;set;}
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Create Relationship
+            modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne(a => a.Category).HasForeignKey(a => a.CategoryId);
+            modelBuilder.Entity<Order>().HasMany(o => o.Products);
+            modelBuilder.Entity<Order>().HasOne(o => o.User);
+            modelBuilder.Entity<User>().HasMany(u => u.Orders).WithOne(o => o.User).HasForeignKey(o => o.UserId);
+
+            //modelBuilder.Seed();
+        }
+    }
+}
